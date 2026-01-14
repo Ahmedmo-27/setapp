1a6c3914807db9a57324216e66e5566ea600fbfd
Ahmedmo-27 <ahmed2303479@miuegypt.edu.eg>
2026-01-15 01:34:00 +0200

    improved mobile view for Floating Badges Component and added lang attribute for index.html

--- src/components/FloatingBadges.jsx
+++ src/components/FloatingBadges.jsx
@@ -6,36 +6,31 @@ const ITEMS = [
     key: "pics",
     src: "/pics.svg",
     alt: "Work with pics",
-    // Moved outward from 17.9% to 12%
-    className: "left-[12%] top-[46px] w-[173px] rotate-[-22deg]",
+    className: "left-[2%] md:left-[12%] top-[40px] md:top-[46px] w-[70px] md:w-[173px] rotate-[-22deg]",
   },
   {
     key: "macpaw",
     src: "/macpaw.svg",
     alt: "MacPaw",
-    // Moved outward from 17.8% to 12%
-    className: "left-[12%] top-[177px] w-[58px] rotate-0",
+    className: "left-[5%] md:left-[12%] top-[155px] md:top-[177px] w-[25px] md:w-[58px] rotate-0",
   },
   {
     key: "pdf",
     src: "/pdf.svg",
     alt: "PDF tools",
-    // Moved outward from 23.4% to 18%
-    className: "left-[18%] top-[170px] w-[142px] rotate-0",
+    className: "left-[12%] md:left-[18%] top-[145px] md:top-[170px] w-[60px] md:w-[142px] rotate-0",
   },
   {
     key: "teamwork",
     src: "/teamwork.svg",
     alt: "Boost teamwork",
-    // Moved outward from 17.6% to 12%
-    className: "left-[12%] top-[338px] w-[227px] rotate-[-5deg]",
+    className: "left-[-2%] md:left-[12%] top-[290px] md:top-[338px] w-[95px] md:w-[227px] rotate-[-5deg]",
   },
   {
     key: "secure",
     src: "/secure.svg",
     alt: "Stay secure",
-    // Moved outward from 17.5% to 12%
-    className: "left-[12%] top-[418px] w-[175px] rotate-[-19deg]",
+    className: "left-[2%] md:left-[12%] top-[360px] md:top-[418px] w-[75px] md:w-[175px] rotate-[-19deg]",
   },
 
   // RIGHT cluster
@@ -43,42 +38,37 @@ const ITEMS = [
     key: "wifi",
     src: "/wifi.svg",
     alt: "Fix WiFi",
-    // Moved outward from 73.9% to 79%
-    className: "left-[79%] top-[64px] w-[201px] rotate-[18deg]",
+    className: "right-[2%] md:left-[79%] top-[55px] md:top-[64px] w-[85px] md:w-[201px] rotate-[18deg]",
   },
   {
     key: "plan",
     src: "/plan.svg",
     alt: "Plan your day",
-    // Moved outward from 69% to 74%
-    className: "left-[74%] top-[124px] w-[169px] rotate-[-20deg]",
+    className: "right-[6%] md:left-[74%] top-[110px] md:top-[124px] w-[70px] md:w-[169px] rotate-[-20deg]",
   },
   {
     key: "converter",
     src: "/converter.svg",
     alt: "Convert",
-    // Moved outward from 77% to 82%
-    className: "left-[82%] top-[235px] w-[155px] rotate-0",
+    className: "right-[2%] md:left-[82%] top-[205px] md:top-[235px] w-[65px] md:w-[155px] rotate-0",
   },
   {
     key: "manage",
     src: "/manage.svg",
     alt: "Manage your Mac",
-    // Moved outward from 70% to 75%
-    className: "left-[75%] top-[336px] w-[230px] rotate-[9deg]",
+    className: "right-[-2%] md:left-[75%] top-[290px] md:top-[336px] w-[95px] md:w-[230px] rotate-[9deg]",
   },
   {
     key: "code",
     src: "/code.svg",
     alt: "Code easier",
-    // Moved outward from 73% to 78%
-    className: "left-[78%] top-[440px] w-[152px] rotate-[-7deg]",
+    className: "right-[5%] md:left-[78%] top-[380px] md:top-[440px] w-[65px] md:w-[152px] rotate-[-7deg]",
   },
 ];
 
 export default function FloatingBadges() {
   return (
-    <div aria-hidden className="hidden md:block pointer-events-none absolute inset-0">
+    <div aria-hidden className="pointer-events-none absolute inset-0 overflow-hidden">
       {ITEMS.map((item) => (
         <img
           key={item.key}
@@ -86,8 +76,8 @@ export default function FloatingBadges() {
           alt={item.alt}
           className={[
             "absolute select-none object-contain",
-            "max-md:scale-[0.85] max-sm:scale-[0.72]",
-            "drop-shadow-[0_18px_35px_rgba(0,0,0,0.55)]",
+            "opacity-80 md:opacity-100", // High opacity for clear mobile view
+            "drop-shadow-[0_8px_16px_rgba(0,0,0,0.35)]",
             item.className,
           ].join(" ")}
           draggable={false}
